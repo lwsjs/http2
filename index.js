1d7f3034a8be03cda33de075b0b2acbc0b3719a2
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-06-17 00:02:18 +0100

    update method names

--- index.js
+++ index.js
@@ -1,77 +1,26 @@
 module.exports = ServerFactory => class Http2ServerFactory extends ServerFactory {
   create (options) {
-    let key = options.key
-    let cert = options.cert
-
-    if (!(key && cert)) {
-      key = this.getDefaultKey()
-      cert = this.getDefaultCert()
-    }
-
-    if (key && cert) {
-      const fs = require('fs')
-      const serverOptions = {
-        key: fs.readFileSync(key),
-        cert: fs.readFileSync(cert)
-      }
-
-      const http2 = require('http2')
-      const server = http2.createServer(serverOptions)
-      return server
+    const fs = require('fs')
+    const https = require('http2')
+    if (options.pfx) {
+      options.pfx = fs.readFileSync(options.pfx)
     } else {
-      throw new Error('http2 server requires a key and cert')
-    }
-
-    server.on('request', (req, res) => {
-      if (req.url === '/') {
-        console.error('DOING PUSH')
-        doPush2(res, '/lib/dojo/dojo.js', 'src/lib/dojo/dojo.js')
-        doPush2(res, '/lib/dijit/dijit.js', 'src/lib/dijit/dijit.js')
-        // const FileSet = require('file-set')
-        // let fileSet = new FileSet('src/script/**/*.js')
-        // for (const file of fileSet.files) {
-        //   // console.log(file)
-        //   doPush2(res, file.replace('src/', '/'), file)
-        // }
-        // fileSet = new FileSet('src/lib/dojo/**/*.js')
-        // for (const file of fileSet.files) {
-        //   // console.log(file)
-        //   doPush2(res, file.replace('src/', '/'), file)
-        // }
-        // fileSet = new FileSet('src/lib/dijit/**/*.js')
-        // for (const file of fileSet.files) {
-        //   console.log(file)
-        //   doPush2(res, file.replace('src/', '/'), file)
-        // }
+      if (!(options.key && options.cert)) {
+        options.key = this.getDefaultKeyPath()
+        options.cert = this.getDefaultCertPath()
       }
-    })
-
-    function doPush (res, url, filename, type = 'application/javascript') {
-      const stream = res.push(url, {
-        status: 200,
-        method: 'GET',
-        request: {
-          accept: '*/*'
-        },
-        response: {
-          'content-type': type
-        }
-      })
-      stream.on('error', console.error)
-      const fs = require('fs')
-      // fs.createReadStream('index.js').pipe(process.stderr)
-      fs.createReadStream(filename).pipe(stream)
+      options.key = fs.readFileSync(options.key)
+      options.cert = fs.readFileSync(options.cert)
     }
-
-    function doPush2 (res, url, filename, type = 'application/javascript') {
-      const push = res.push(url)
-      push.writeHead(200, {
-        'content-type': type
-      })
-      const fs = require('fs')
-      fs.createReadStream(filename).pipe(push)
-    }
-
-    return server
+    return https.createServer(options)
   }
+}
+
+function doPush (res, url, filename, type = 'application/javascript') {
+  const push = res.push(url)
+  push.writeHead(200, {
+    'content-type': type
+  })
+  const fs = require('fs')
+  fs.createReadStream(filename).pipe(push)
 }
